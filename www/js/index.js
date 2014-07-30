93f352da912af4378e2deb2ea5b9c64109d58cf5
alexiscps <alexis.cps@gmail.com>
2014-07-30 17:25:43 +0300

    7

--- www/js/index.js
+++ www/js/index.js
@@ -37,6 +37,7 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -53,6 +54,3 @@ $(document).ready(function(){
         alert('Clicked');
     });
 });
-
-
-
